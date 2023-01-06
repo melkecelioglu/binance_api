16b771701599f145fc9f81a3c1e33fc11085c149
melkecelioglu <melkecelioglu@gmail.com>
2023-01-06 06:18:29 +0100

    [ADD]-SetUp Flask Backend

--- ta.py
+++ ta.py
@@ -1,6 +1,21 @@
 import numpy
 import talib
+from numpy import genfromtxt
 
-close=numpy.random.random(100)
-
+my_data = genfromtxt('15minutes.csv', delimiter=',')
+print(my_data)
+close= my_data[:,4]
 print(close)
+# close=numpy.random.random(100)
+
+# print(close)
+
+# moving_average = talib.SMA(close, timeperiod=10) #30 timeperiod first elements
+# print(moving_average)
+
+#relatice strenght test
+rsi = talib.RSI(close)
+print(rsi)
+
+
+#unixtimestamp.com/index.php
