2bf77f05f381a4788af56c55dd4460478b2e96e3
melkecelioglu <melkecelioglu@gmail.com>
2023-01-11 23:47:22 +0100

    [ADD]-Account Balances, Exchange Info, and Buy Execution

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+import config, csv 
+from binance.client import Client
+
+
 app = Flask(__name__)
+
+client = Client(config.API_KEY, config.API_SECRET, tld='com')
+
 
 @app.route('/')
 def index():
     title ='CoinView'
-    return render_template('index.html', title=title)
+
+    info = client.get_account()
+    balances= info['balances']
+   # print(balances)
+#    print(info)
+#    print(client)
+    return render_template('index.html', title=title, my_balances=balances)
 
 @app.route('/buy2')
 def buy():
