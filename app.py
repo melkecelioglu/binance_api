49fd338c987e4f02a1eb84fbc011474ce78fda1b
melkecelioglu <melkecelioglu@gmail.com>
2023-01-12 18:13:58 +0100

    [ADD]-Account Balances, Exchange Info, and Buy Execution

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
 import config, csv 
 from binance.client import Client
-
+from binance.enums import *
 
 app = Flask(__name__)
+app.secret_key= b'sdkjfhjsdk"F4Q8zec]/'
 
 client = Client(config.API_KEY, config.API_SECRET, tld='com')
 
@@ -12,16 +13,29 @@ client = Client(config.API_KEY, config.API_SECRET, tld='com')
 def index():
     title ='CoinView'
 
-    info = client.get_account()
-    balances= info['balances']
-   # print(balances)
-#    print(info)
-#    print(client)
-    return render_template('index.html', title=title, my_balances=balances)
+    account = client.get_account()
+    balances= account['balances']
+    exchange_info = client.get_exchange_info()
+    symbols = exchange_info['symbols']
+    return render_template('index.html', title=title, my_balances=balances, symbols = symbols)
 
-@app.route('/buy2')
+@app.route('/buy', methods=['POST'])
 def buy():
-    return 'buy'
+    print(request.form)
+    try:
+        order = client.create_order(
+            symbol=request.form['symbol'],
+            side=SIDE_BUY,
+            type=ORDER_TYPE_MARKET,
+            timeInFroce=TIME_IN_FORCE_GTC,
+            quantity=request.form['quantity'])
+    except Exception as e:
+        flash(e.message, "error")
+    return redirect('/')
+
+# @app.route('/buy2')
+# def buy():
+#     return 'buy'
 
 @app.route('/sell')
 def sell():
