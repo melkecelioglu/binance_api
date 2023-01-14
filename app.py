9f0260e312a744db65d6148450b9aa3e2bea5600
melkecelioglu <melkecelioglu@gmail.com>
2023-01-14 22:37:10 +0100

    [ADD]-Real-Time Candlestick Charts using Websockets and JavaScript

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, flash, redirect, jsonify
 import config, csv 
 from binance.client import Client
 from binance.enums import *
@@ -45,3 +45,20 @@ def sell():
 @app.route('/settings')
 def settings():
     return 'settings'
+
+@app.route('/history')
+def history():
+    candlesticks=client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_15MINUTE, "1 July, 2020","12 Jul, 2020")
+
+    processed_candlesticks= []
+    for data in candlesticks:
+        candlestick= {
+            "time":data[0]/1000,
+            "open":data[1],
+            "high":data[2],
+            "low":data[3],
+            "close":data[4],
+        },
+        processed_candlesticks.append(candlestick)
+
+    return jsonify(processed_candlesticks)
